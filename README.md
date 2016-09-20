d0107abf0cf25d17ce2e16631f5f0e3e18e15612
Nick Doiron <ndoiron@mapmeld.com>
2016-09-20 09:26:02 -0400

    Readme

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 Visualize letters which make up an Arabic word
 
+<img src="https://mapmeld.github.io/arabic-letters/sample.png"/>
 
+## Issues
+
+Currently can't differentiate pixels which were part of a previous ending-form and
+make it into the final printed-form.
+
+I tried removing letters sequentially instead of adding them, but then we get the
+ending-form of each letter, instead of the printed-form.
 
 ## License
 
--- segmentation.js
+++ segmentation.js
@@ -6,51 +6,64 @@ var ctx2 = staging2.getContext('2d');
 var canv = document.getElementById('canvas');
 var output = canv.getContext('2d');
 
-function clear() {
-  ctx.fillStyle = '#fff';
-  ctx2.fillStyle = '#fff';
-  ctx.fillRect(0, 0, 200, 100);
-  ctx2.fillRect(0, 0, 200, 100);
+function clear(specialctx) {
+  if (specialctx) {
+    specialctx.fillStyle = '#fff';
+    specialctx.fillRect(0, 0, 300, 140);
+  } else {
+    clear(ctx);
+    clear(ctx2);
+  }
 }
 
-var word = 'العربية';
 ctx.font = '84px serif';
 ctx2.font = '84px serif';
 var colors = ['red', 'orange', 'green', 'blue', 'purple'];
-var oldWidth = 0;
 
-for (var w = 0; (w + 1) < word.length + 1; w++) {
-  clear();
-  ctx.fillStyle = colors[w % colors.length];
-  ctx2.fillStyle = colors[w % colors.length];
+function processWord(word) {
+  var oldWidth = 0;
 
-  // even though Arabic is right-to-left
-  // the X coordinate is the left-most point of the word or character
-  ctx.fillText(word.substring(0, w + 1), 10, 70);
+  for (var w = 0; (w + 1) < word.length + 1; w++) {
+    clear();
+    ctx.fillStyle = colors[w % colors.length];
+    ctx2.fillStyle = colors[w % colors.length];
 
-  var newWidth = ctx.measureText(word.substring(0, w + 1)).width;
+    // even though Arabic is right-to-left
+    // the X coordinate is the left-most point of the word or character
+    ctx.fillText(word.substring(0, w + 1), 10, 70);
 
-  ctx2.fillText(word.substring(0, w), 10 + newWidth - oldWidth, 70);
-  oldWidth = newWidth;
+    var newWidth = ctx.measureText(word.substring(0, w + 1)).width;
 
-  var lastChars = ctx2.getImageData(0, 0, 300, 140).data;
-  var newChars = ctx.getImageData(0, 0, 300, 140).data;
+    ctx2.fillText(word.substring(0, w), 10 + newWidth - oldWidth, 70);
+    oldWidth = newWidth;
 
-  for (var x = 299; x >= 0; x--) {
-    for (var y = 0; y < 140; y++) {
-      var r = lastChars[(y * 300 + x) * 4];
-      var g = lastChars[(y * 300 + x) * 4 + 1];
-      var b = lastChars[(y * 300 + x) * 4 + 2];
+    var lastChars = ctx2.getImageData(0, 0, 300, 140).data;
+    var newChars = ctx.getImageData(0, 0, 300, 140).data;
 
-      var r2 = newChars[(y * 300 + x) * 4];
-      var g2 = newChars[(y * 300 + x) * 4 + 1];
-      var b2 = newChars[(y * 300 + x) * 4 + 2];
+    for (var x = 299; x >= 0; x--) {
+      for (var y = 0; y < 140; y++) {
+        var r = lastChars[(y * 300 + x) * 4];
+        var g = lastChars[(y * 300 + x) * 4 + 1];
+        var b = lastChars[(y * 300 + x) * 4 + 2];
 
-      if (r != r2 || g != g2 || b != b2) {
-        // modified pixel
-        output.fillStyle = 'rgb(' + [r2, g2, b2].join(',') + ')';
-        output.fillRect(270 - Math.round(newWidth - x), y + 20, 1, 1);
+        var r2 = newChars[(y * 300 + x) * 4];
+        var g2 = newChars[(y * 300 + x) * 4 + 1];
+        var b2 = newChars[(y * 300 + x) * 4 + 2];
+
+        if (r != r2 || g != g2 || b != b2) {
+          // modified pixel
+          output.fillStyle = 'rgb(' + [r2, g2, b2].join(',') + ')';
+          output.fillRect(270 - Math.round(newWidth - x), y + 20, 1, 1);
+        }
       }
     }
   }
 }
+
+processWord('العربية');
+
+document.getElementById('try').onclick = function() {
+  clear(output);
+  var word = document.getElementById('wordEntry').value;
+  processWord(word);
+};
